c30d3a09d4fcacc4c1b2777b0719b4996d41cc6d
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-05-27 22:28:36 +0300

    Images can be shown to admin

--- app/src/main/java/com/example/communityreport/UploadSuccess.kt
+++ app/src/main/java/com/example/communityreport/UploadSuccess.kt
@@ -1,11 +1,26 @@
 package com.example.communityreport
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class UploadSuccess : AppCompatActivity() {
+    private var showBtn: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload_success)
+        showBtn = findViewById(R.id.button2)
+
+        showBtn?.setOnClickListener {
+            val intent = Intent(this, ShowActivity::class.java)
+
+
+
+            startActivity(intent)
+        }
     }
+
+
 }
