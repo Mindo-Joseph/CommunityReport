b059fe309add7717695254c88a2b5b6377f94ad7
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2021-05-30 07:21:12 +0300

    Attach location to each image upload

--- app/src/main/java/com/example/communityreport/Model.java
+++ app/src/main/java/com/example/communityreport/Model.java
@@ -3,24 +3,28 @@ package com.example.communityreport;
 public class Model {
     private String imageUrl;
     private String imageName;
+    private Object location;
     public Model () {
 
     }
 
-    public Model(String imageUrl, String imageName){
+    public Model(String imageUrl, String imageName, Object location){
         this.imageUrl = imageUrl;
         this.imageName = imageName;
+        this.location = location;
 
     }
     public String getImageUrl() {
         return imageUrl;
     }
     public String getImageName(){return imageName;}
+    public Object getLocation() {return location;}
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
 
 
     }
     public void setImageName(String imageName){ this.imageName = imageName;}
+    public void setLocation(Object location){ this.location = location;}
 
 }
